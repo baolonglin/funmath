55f7986f053ac347b84414c82b2b5470a649e9d2
Robin Lin <lbl52001@gmail.com>
2016-10-28 21:45:21 +0200

    add menu

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget, QMainWindow, QAction, qApp
 from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
 
 class Example(QMainWindow):
 
@@ -13,19 +14,37 @@ class Example(QMainWindow):
 
     def initUI(self):
 
-        
         qbtn = QPushButton('Quit', self)
-        qbtn.clicked.connect(QCoreApplication.instance().quit)
+        qbtn.clicked.connect(qApp.quit)
         qbtn.resize(qbtn.sizeHint())
         qbtn.setToolTip('This is a <b>QPushButton</b> widget')
         qbtn.move(50, 50)
 
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        copyAction = QAction(QIcon('copy.png'), '&Copy', self)
+        copyAction.setShortcut('Ctrl+C')
+        copyAction.setStatusTip('Copy selected')
+        copyAction.triggered.connect(self.copy)
+        
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+        editMenu = menubar.addMenu('&Edit')
+        editMenu.addAction(copyAction)
+        
         self.statusBar().showMessage('Ready')
         self.resize(250, 150)
         self.center()
         self.setWindowTitle('Example')
         self.show()
 
+    def copy(self):
+        pass
+    
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
