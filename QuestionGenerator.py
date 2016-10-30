f2b71c9ade23557eb318496366f752969c61a90f
Robin Lin <lbl52001@gmail.com>
2016-10-30 20:53:32 +0100

    add generator algorithm with bug :)

--- QuestionGenerator.py
+++ QuestionGenerator.py
@@ -1,12 +1,81 @@
 
+from enum import Enum
+import random
+
+class Operator(Enum):
+    plus = 1
+    minus = 2
+    multiply = 3
+    divide = 4
+
+    def __str__(self):
+        if self.value == 1:
+            return "+"
+        elif self.value == 2:
+            return '-'
+        elif self.value == 3:
+            return '*'
+        elif self.value == 4:
+            return '/'
+
 class QuestionGenerator:
 
     def __init__(self, settings):
         self.settings = settings
+        self.supportedOp = []
+        self.generatedQuestions = []
+        self.answers = []
+        self.current_question_idx = 0
+
+        self.initSupportedOp()
         self.initQuestion()
 
+    def initSupportedOp(self):
+        if self.settings.isPlusEnable():
+            self.supportedOp.append(Operator.plus)
+        if self.settings.isMinusEnable():
+            self.supportedOp.append(Operator.minus)
+        if self.settings.isPlusEnable():
+            self.supportedOp.append(Operator.multiply)
+        if self.settings.isDivideEnable():
+            self.supportedOp.append(Operator.divide)
+
+    def getRandNum(self):
+        return random.randint(self.settings.getMinNum(),
+                              self.settings.getMaxNum())
+    def getRandOp(self):
+        return self.supportedOp[
+            random.randint(0, len(self.supportedOp) - 1)]
+
     def initQuestion(self):
-        pass
-    
-    def next(self):
-        pass
+        for q_idx in range(self.settings.getQuestionNum()):
+            question = []
+            for i_idx in range(self.settings.getOperatorNum()):
+                question.append(self.getRandNum())
+                question.append(self.getRandOp())
+            question.append(self.getRandNum())
+            self.generatedQuestions.append(question)
+            self.answers.append(-1)
+
+    def curr(self):
+        return self.generatedQuestions[self.current_question_idx], self.answers[self.current_question_idx]
+
+    def updateCurrentQuestionAnswer(self, answer):
+        if self.current_question_idx < len(self.generatedQuestions):
+            self.answers[self.current_question_idx] = answer
+
+    def previous(self, answer):
+        self.updateCurrentQuestionAnswer(answer)
+        self.current_question_idx -= 1
+        if self.current_question_idx > 0:
+            return self.curr()
+        self.current_question_idx += 1
+        return None
+
+    def next(self, answer):
+        self.updateCurrentQuestionAnswer(answer)
+        self.current_question_idx += 1
+        if self.current_question_idx < len(self.generatedQuestions):
+            return self.curr()
+        self.current_question_idx -= 1
+        return None
--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMessageBox, QDesktopWidget, \
-    QMainWindow, QAction, qApp
+    QMainWindow, QAction, qApp, QHBoxLayout, QLabel, QLineEdit, QWidget
 from PyQt5.QtCore import Qt, pyqtSignal, QObject
 from PyQt5.QtGui import QIcon
 
 from SettingsDialog import SettingsDialog
 from Settings import Settings
+from QuestionGenerator import QuestionGenerator
 
 
 class Communicate(QObject):
@@ -20,6 +21,8 @@ class Main(QMainWindow):
         super().__init__()
         self.initUI()
         self.settings = Settings()
+        self.questions = None
+        self.started = False
 
     def initUI(self):
         exitAction = QAction(QIcon('exit.png'), '&Exit', self)
@@ -32,15 +35,20 @@ class Main(QMainWindow):
         settingsAction.setStatusTip('Change settings')
         settingsAction.triggered.connect(self.settings)
 
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(exitAction)
-        editMenu = menubar.addMenu('&Edit')
-        editMenu.addAction(settingsAction)
+        self.startAction = QAction(QIcon('start.png'), '&Start', self)
+        self.startAction.setShortcut(Qt.Key_Enter)
+        self.startAction.setStatusTip('Start')
+        self.startAction.triggered.connect(self.start)
+
+        stopAction = QAction(QIcon('stop.png'), 'Stop', self)
+        stopAction.setStatusTip('Stop')
+        stopAction.triggered.connect(self.stop)
 
         toolbar = self.addToolBar('Exit')
         toolbar.addAction(exitAction)
         toolbar.addAction(settingsAction)
+        toolbar.addAction(self.startAction)
+        toolbar.addAction(stopAction)
 
         self.c = Communicate()
         self.c.closeApp.connect(self.close)
@@ -51,6 +59,57 @@ class Main(QMainWindow):
         self.setWindowTitle('Fun Math')
         self.show()
 
+    def stop(self):
+        self.showResult()
+        self.questions = None
+
+    def getCurrAnswer(self):
+        t = self.answerEdit.text()
+        if t.strip().isnumeric():
+            return int(t.strip())
+        return -1
+
+    def showQuestion(self, question):
+        if question:
+            q = question[0]
+            a = question[1]
+            grid = QHBoxLayout()
+            for e in q:
+                grid.addWidget(QLabel(str(e)))
+            grid.addWidget(QLabel('='))
+            self.answerEdit = QLineEdit()
+            if a != -1:
+                self.answerEdit.setText(str(a))
+            grid.addWidget(self.answerEdit)
+            mainWidget = QWidget()
+            mainWidget.setLayout(grid)
+            self.setCentralWidget(mainWidget)
+        else:
+            reply = QMessageBox.question(self, 'Message',
+                                         "Do you want to submit?",
+                                         QMessageBox.Yes | QMessageBox.No,
+                                         QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                self.showResult()
+
+    def showResult(self):
+        pass
+
+    def hideQuestion(self):
+        self.setLayout(QHBoxLayout())
+
+    def start(self):
+        if not self.questions:
+            self.questions = QuestionGenerator(self.settings)
+        if not self.started:
+            self.showQuestion(self.questions.curr())
+            self.started = True
+            self.startAction.setIcon(QIcon('pause.png'))
+        else:
+            self.started = False
+            self.hideQuestion()
+            self.startAction.setIcon(QIcon('start.png'))
+
     def settings(self):
         settingGui = SettingsDialog(self, self.settings)
         settingGui.exec_()
@@ -58,6 +117,10 @@ class Main(QMainWindow):
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Escape:
             self.close()
+        elif e.key() == Qt.Key_Left:
+            self.showQuestion(self.questions.previous(self.getCurrAnswer()))
+        elif e.key() == Qt.Key_Right:
+            self.showQuestion(self.questions.next(self.getCurrAnswer()))
 
     def mousePressEvent(self, event):
         self.c.closeApp.emit()
