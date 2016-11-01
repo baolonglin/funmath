c42b535cef688be1be1bdba3e00a5e411baec8f6
Robin Lin <lbl52001@gmail.com>
2016-11-01 15:50:27 +0100

    add negative filter

--- QuestionGenerator.py
+++ QuestionGenerator.py
@@ -8,6 +8,15 @@ class Operator(Enum):
     multiply = 3
     divide = 4
 
+    def highPriorityThen(self, op2):
+        if self in [Operator.plus, Operator.minus]:
+            if op2 in [Operator.plus, Operator.minus]:
+                return True
+            else:
+                return False
+        else:
+            return True
+
     def __str__(self):
         if self.value == 1:
             return "+"
@@ -47,18 +56,74 @@ class QuestionGenerator:
         return self.supportedOp[
             random.randint(0, len(self.supportedOp) - 1)]
 
-    def validateQuestion(self, question):
-        pass
+    def noNegative(self, question):
+        parseIntoPostFix = []
+        currOp = None
+        for item in question:
+            if item in Operator:
+                if not currOp:
+                    currOp = item
+                else:
+                    if currOp.highPriorityThen(item):
+                        parseIntoPostFix.append(currOp)
+                        currOp = item
+                    else:
+                        parseIntoPostFix.append(item)
+            else:
+                parseIntoPostFix.append(item)
+        if currOp:
+            parseIntoPostFix.append(currOp)
+
+        exprStack = []
+        while len(parseIntoPostFix) > 0:
+            item = parseIntoPostFix.pop(0)
+
+            if item in Operator:
+                if item is Operator.minus:
+                    if len(exprStack) > 1:
+                        tmp = exprStack.pop()
+                        result = exprStack.pop() - tmp
+                        if result < 0:
+                            return False
+                        exprStack.append(result)
+                    else:
+                        return False
+                elif item is Operator.divide:
+                    if len(exprStack) > 1:
+                        tmp = exprStack.pop()
+                        exprStack.append(exprStack.pop() / tmp)
+                else:
+                    if len(exprStack) > 1:
+                        exprStack.append(eval(str(exprStack.pop()) + str(item) + str(exprStack.pop())))
+                    else:
+                        return False
+            else:
+                exprStack.append(item)
+        return True
+
+    def isDuplicate(self, question):
+        return question in self.generatedQuestions
+
+    def filterQuestion(self, question):
+        ret = True
+        if not self.settings.isSupportNegative():
+            ret = ret and self.noNegative(question)
+
+        ret = ret and not self.isDuplicate(question)
+        return ret
 
     def initQuestion(self):
         for q_idx in range(self.settings.getQuestionNum()):
-            question = []
-            for i_idx in range(self.settings.getOperatorNum()):
+            while True:
+                question = []
+                for i_idx in range(self.settings.getOperatorNum()):
+                    question.append(self.getRandNum())
+                    question.append(self.getRandOp())
                 question.append(self.getRandNum())
-                question.append(self.getRandOp())
-            question.append(self.getRandNum())
-            self.generatedQuestions.append(question)
-            self.answers.append(-1)
+                if self.filterQuestion(question):
+                    self.generatedQuestions.append(question)
+                    self.answers.append(-1)
+                    break
 
     def curr(self):
         return self.generatedQuestions[self.current_question_idx], self.answers[self.current_question_idx]
